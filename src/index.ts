d31c21d7aed698517833a1f7c498131ce17a84b0
Clikuki <lanzvasquez24@gmail.com>
2024-03-30 22:44:07 +0800

    connected basic logic to ui

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ type GAME_STATES = 'PLAY' | 'DRAW' | PLAYER;
 let gameState: GAME_STATES = 'PLAY';
 let isPlayerX = true;
 let mustPlayAt = NaN;
+const ignoredBoardIndices = new Set<number>();
 const board: string[][] = [
 	// Row 1
 	[],
@@ -75,15 +76,56 @@ function isBoardDraw(board: string[]): boolean {
 	return true;
 }
 
-function markBoard(outer: number, inner: number): GAME_STATES {
+function markBoard(outer: number, inner: number): void {
 	let mark = isPlayerX ? PLAYER.FIRST : PLAYER.LAST;
 	board[outer][inner] = mark;
-
-	if (isBoardDraw(board[inner])) {
-		outer = NaN;
-	} else {
-		outer = inner;
-	}
-
-	return (gameState = getOuterBoardState(outer));
+	innerTileEls[outer][inner].setAttribute('data-mark', mark);
 }
+
+const boardEl = document.querySelector('.board')!;
+const outerTileEls = Array.from(boardEl.children);
+const innerTileEls = outerTileEls.map((el) => {
+	const smallContainer = el.querySelector('.container') as HTMLElement;
+	return Array.from(smallContainer.children);
+});
+
+innerTileEls.forEach((innerTiles, i) => {
+	innerTiles.forEach((innerEl, j) => {
+		innerEl.addEventListener('click', () => {
+			if (
+				(isNaN(mustPlayAt) && ignoredBoardIndices.has(i)) ||
+				(!isNaN(mustPlayAt) && i !== mustPlayAt)
+			) {
+				return;
+			}
+
+			markBoard(i, j);
+			isPlayerX = !isPlayerX;
+
+			const innerWinner = getBoardWinner(board[i]);
+			if (innerWinner) {
+				outerTileEls[i].setAttribute('data-mark', innerWinner);
+			}
+			if (innerWinner || isBoardDraw(board[i])) {
+				outerTileEls[i].removeAttribute('data-enabled');
+				ignoredBoardIndices.add(i);
+			}
+
+			if (ignoredBoardIndices.has(j)) {
+				mustPlayAt = NaN;
+				for (let k = 0; k < 9; k++) {
+					if (ignoredBoardIndices.has(k)) continue;
+					outerTileEls[k].setAttribute('data-enabled', '');
+				}
+			} else {
+				mustPlayAt = j;
+				for (const outer of outerTileEls) {
+					outer.removeAttribute('data-enabled');
+				}
+				outerTileEls[j].setAttribute('data-enabled', '');
+			}
+
+			gameState = getOuterBoardState(i);
+		});
+	});
+});
